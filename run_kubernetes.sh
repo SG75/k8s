8d42600be0e9ad7620b19e9bac252ee14a0adf19
Gururaj S <gururaj.sudhindra@gmail.com>
2020-03-31 18:53:07 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -16,9 +16,9 @@ done
 echo "POD is Running"
 
 # Forward the container port to a host
-kubectl port-forward --address 0.0.0.0 pod/mlms-app 80:80&
+kubectl port-forward --address 0.0.0.0 pod/mlms-app 80:80
 
 # Shows logs of containers
 
-kubectl logs $(kubectl get pods |head -2|awk 'NR>1{ print $1}'|tr -d '\n')
+#kubectl logs $(kubectl get pods |head -2|awk 'NR>1{ print $1}'|tr -d '\n')
 
