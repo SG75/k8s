3d61ec75fc94baa8206589f4a20d118f29e2f297
Gururaj S <gururaj.sudhindra@gmail.com>
2020-04-01 07:49:05 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
 
 dockerpath=sg75/mlms:v1
-docker login
-docker push $dockerpath
+echo "Docker image and tag $dockerpath"
 
+echo "=================================="
+
+echo "adding an descriptive tag to the exisitng image"
+
+docker tag $dockerpath sg75/mlms:version1
+
+echo "=========================================================================="
+
+echo "listing images look for sg75/mlms:version1"
+
+echo "     "
+
+docker images
+
+echo "=========================================================================="
+echo "   "
+
+echo "logging to dockerhub"
+
+docker login
+
+echo "============================================================================="
+echo "     "
+
+echo "pushing the newly tagged image to dockerhub"
+echo "    "
+echo "============================================================================="
+
+docker push sg75/mlms:version1
